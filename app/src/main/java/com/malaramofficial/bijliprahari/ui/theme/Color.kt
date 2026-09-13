6a41b8b3f8ab75e24bbdc953d72e4db09447b920
malaramofficial <malaramofficial@gmail.com>
2026-09-13 12:56:12 +0530

    Polish Bijli Prahari UI theme

---
+++ app/src/main/java/com/malaramofficial/bijliprahari/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.malaramofficial.bijliprahari.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Violet = Color(0xFF6D28D9)
+val VioletDark = Color(0xFF4C1D95)
+val Yellow = Color(0xFFFFC107)
+val Green = Color(0xFF16A34A)
+val Red = Color(0xFFDC2626)
