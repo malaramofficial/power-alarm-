d153e0f3d1d479a7838ea81302b095c4877e13a2
malaramofficial <malaramofficial@gmail.com>
2026-09-13 13:06:48 +0530

    Bump Bijli Prahari release version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android { namespace = "com.malaramofficial.bijliprahari"; compileSdk = 35
-    defaultConfig { applicationId = "com.malaramofficial.bijliprahari"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+    defaultConfig { applicationId = "com.malaramofficial.bijliprahari"; minSdk = 24; targetSdk = 35; versionCode = 2; versionName = "1.1.0" }
     buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
